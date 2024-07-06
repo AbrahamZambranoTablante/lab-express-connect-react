d8de90225b7ff15faac3b1745ac88747417fd1d0
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-05 20:43:01 -0400

    lab finished

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ export default function Home () {
     return (
         <>
             <h1>CAPTAIN'S LOG</h1>
+            <p>(sorry about lack of CSS but i need to start with others assignments)</p>
         </>
     )
 }
